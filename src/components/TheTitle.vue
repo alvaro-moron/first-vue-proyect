1b804f0bb9be3cdeacdf194aefce0c85666c2664
alvaro moron <alvaro.moron@alebateducation.com>
2025-01-25 21:14:02 +0100

    exercise 2 completed

---
+++ src/components/TheTitle.vue
@@ -0,0 +1,14 @@
+<script lang="ts" setup></script>
+<template>
+  <section class="title-container">
+    <h2 class="title-container__subTitle">This is the main subtitle</h2>
+  </section>
+</template>
+<style scoped lang="scss">
+  .title-container {
+    &__subTitle {
+      margin: 2em 0;
+      text-align: center;
+    }
+  }
+</style>
